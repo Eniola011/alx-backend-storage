65ffbe1a0dd600bc4682f290559d953d7fa9492b
Eniola011 <yettyagbalu@gmail.com>
2024-03-23 09:18:48 +0000

    average scores

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,36 +2,14 @@
 -- that computes and store the average score for a student.
 -- Note: An average score can be a decimal
 
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER //
-
-CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT)
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE total_projects INT;
-
-    -- Compute total score for the user
-    SELECT SUM(score)
-    INTO total_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Compute total number of projects for the user
-    SELECT COUNT(*)
-    INTO total_projects
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Compute and store the average score for the user
-    IF total_projects > 0 THEN
-        UPDATE users
-        SET average_score = total_score / total_projects
-        WHERE id = user_id;
-    ELSE
-        UPDATE users
-        SET average_score = 0
-        WHERE id = user_id;
-    END IF;
-END;
+    DECLARE avg_score FLOAT;
+    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
+END
 //
-
 DELIMITER ;
