278b6ba118e7d83bf10aa1edfffb82bb90260b48
Eniola011 <yettyagbalu@gmail.com>
2024-03-26 09:04:18 -0700

    top 10 of the most present IPs in the collection nginx of the database logs

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -10,29 +10,36 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_db = client.logs.nginx
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    method_counts = {method: nginx_db.count_documents({
-        "method": method}) for method in methods}
 
-    # Number of documents in the collection
     total_logs = nginx_db.count_documents({})
+    print(f'{total_logs} logs')
+
+    print("Methods:")
+    for method in methods:
+        count_method = nginx_db.count_documents({'method': method})
+        print(f'\tmethod {method}: {count_method}')
 
     # Number of documents with method=GET and path=/status
     status_check_count = nginx_db.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
 
-    # Calculate the top 10 most present IPs
-    top_ips = nginx_db.aggregate([
-        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+    # Calculate and Print the top 10 most present IPs
+    print("IPs:")
+    ips = nginx_db.aggregate([
+        {"$group":
+            {
+                "_id": "$ip",
+                "count": {"$sum": 1}
+            }
+        },
         {"$sort": {"count": -1}},
         {"$limit": 10},
-        {"$project": {"_id": 0, "ip": "$_id", "count": 1}}
+        {"$project": {
+            "_id": 0,
+            "ip": "$_id",
+            "count": 1
+        }}
     ])
 
-    # Printing the stats
-    print(f'{total_logs} logs')
-    print("Methods:")
-    for method, count in method_counts.items():
-        print(f"\tmethod {method}: {count}")
-    print(f"{status_check_count} status check")
-    print("IPs")
-    for ip in top_ips:
+    for ip in ips:
         print(f"\t{ip.get('ip')}: {ip.get('count')}")
